116bedb1fbb3be85aa8a2f08c5c99c72e1b16ba5
JMonono <julian.monono@gmail.com>
2014-10-26 02:38:22 +0000

    Added 'what' to rootScope and tweaked controllers

--- www/js/whatcontroller.js
+++ www/js/whatcontroller.js
@@ -1,6 +1,9 @@
 angular.module('starter.controllers')
-.controller('WhatCtrl', function($scope, $stateParams) {
+.controller('WhatCtrl', function($scope, $rootScope, $stateParams) {
   console.log($stateParams)
 
   $scope.what = $stateParams.whatId;
+
+  $rootScope.what = {what: $stateParams.whatId};
+  console.log($rootScope.what);
 });
