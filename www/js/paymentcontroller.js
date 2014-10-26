9ec261d0d7a39ee35b764bfb88c7462946d61656
Alan Gorton <dev.code1up@googlemail.com>
2014-10-26 02:58:32 +0000

    payments

--- www/js/paymentcontroller.js
+++ www/js/paymentcontroller.js
@@ -1,4 +1,40 @@
-angular.module('starter.controllers')
-.controller('PaymentCtrl', function($scope) {
-  
+angular.module("starter.controllers")
+.controller("PaymentCtrl", function ($scope, $rootScope, $timeout, $state, $ionicLoading, $ionicPopup) {
+    var launchPingitApp = function () {
+        $scope.showPingit = function () {
+            document.location = "pingit://00/07985222260";
+        };
+    };
+
+    var showThankyouMessage = function () {
+        var popup = $ionicPopup.alert({
+            title: "Thanks",
+            template: "You're awesome!"
+        }).then(function () {
+            $state.go("app.social");
+        });
+    };
+
+    var fakePingitApi = function () {
+        $ionicLoading.show({
+            template: 'Giving...'
+        });
+
+        $timeout(function () {
+            $ionicLoading.hide();
+            showThankyouMessage();
+        }, 2000);
+    };
+
+    $scope.pingit = function () {
+        if ($rootScope !== "Clothing") {
+            launchPingitApp();
+        } else {
+            fakePingitApi();
+        }
+    };
+
+    $scope.goHome = function () {
+        $state.go("^.home");
+    };
 });
