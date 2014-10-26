2daf105d416dec5a658c4b3d433a9ac9ab65436b
JMonono <julian.monono@gmail.com>
2014-10-26 10:14:41 +0000

    Minor what page changes

--- www/js/whatcontroller.js
+++ www/js/whatcontroller.js
@@ -6,4 +6,46 @@ angular.module('starter.controllers')
 
   $rootScope.what = {what: $stateParams.whatId};
   console.log($rootScope.what);
+
+  var getViewModelFromWhat = function (what) {
+        switch (what) {
+            case "Food":
+                return {
+                    what: "Food",
+                    icon: "ion-pizza",
+                    coffees: [ 0, 1 ],
+                    beers: [ 0, 1, 2 ],
+                    mags: [ 0, 1, 2, 3 ],
+                    narrative: "A gift of food is the equivalent of...",
+                    minValue: 2,
+                    maxValue: 4
+                };
+
+            case "Shelter":
+                return {
+                    what: "Shelter",
+                    icon: "ion-home",
+                    coffees: [0, 1, 2 ],
+                    beers: [ 0, 1, 2 ],
+                    mags: [ 0, 1, 2, 3 ],
+                    narrative: "A gift of shelter is the equivalent of...",
+                    minValue: 3,
+                    maxValue: 5
+             };
+
+            default:
+                return {
+                    what: "Clothing",
+                    icon: "ion-person-stalker",
+                    coffees: [0, 1, 2, 3, 4, 5 ],
+                    beers: [ 0, 1, 2 ],
+                    mags: [ 0, 1, 2, 3 ],
+                    narrative: "A gift of clothing is the equivalent of...",
+                    minValue: 8,
+                    maxValue: 8
+                };
+        }
+	};
+
+	$scope.vm = getViewModelFromWhat($rootScope.what);
 });
